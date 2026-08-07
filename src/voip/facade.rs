534054f63765fbd28eb423bb2034b1dc7a7add79
João Lucas <55464917+jlucaso1@users.noreply.github.com>
2026-08-07 13:08:20 -0300

    perf(client): hoist write-once state out of async mutexes (#1227)

--- src/voip/facade.rs
+++ src/voip/facade.rs
@@ -3776,7 +3776,7 @@ mod tests {
             NoiseCipher::new(&key).expect("key"),
             NoiseCipher::new(&key).expect("key"),
         );
-        *client.noise_socket.lock().await = Some(Arc::new(noise_socket));
+        *client.noise_socket.lock().unwrap() = Some(Arc::new(noise_socket));
     }
 
     struct GatedSendTransport {
@@ -3975,7 +3975,6 @@ mod tests {
         let group = Jid::new("120363000000000001", Server::Group);
         client
             .get_group_cache()
-            .await
             .insert(
                 group.clone(),
                 Arc::new(GroupInfo::new(
